20f69c5ac520e9cd12dab4b69923c202801d123e
Claire Haderlie <clairehaderlie@gmail.com>
2019-09-28 16:03:12 -0600

    Made homepage

--- web/week02/ponder02/homepage.js
+++ web/week02/ponder02/homepage.js
@@ -1,3 +1,3 @@
 function changePhoto() {
-    document.getElementById("profile_photo").innerHTML = '<img id="profile_photo" src="engagement_photo.jpg" alt="Claire and Alex Hocker" width="153" height="194" >';
+    document.getElementById("profile_photo").src = "engagement_photo.jpg";
    }
