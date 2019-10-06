6cf5767a8c6d9954c2e713f627484a1f8689e3e9
Claire Haderlie <clairehaderlie@gmail.com>
2019-10-05 21:54:11 -0600

    Ponder 03

--- web/week03/ponder03/view_cart.php
+++ web/week03/ponder03/view_cart.php
@@ -17,10 +17,36 @@ require 'start_session.php';
         array_push($_SESSION['products'], $product);
     }
 
+   
     ?>
-    <form method="post" action="checkout.php">
-        <input type="Submit" value="Continue with Purchase">
-    </form>
+    <table>
+        <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Radius of Planet</th>
+            <th>Inhabitants</th>
+            <th>Price</th>
+        </tr>
+        <?php
+         $total = 0;
+         foreach ($_SESSION['products'] as $product1) {
+             foreach ($_SESSION['all_products'] as $product2) {
+                 if (strcmp($product1, $product2->planet) == 0) {
+                    echo "<tr> <td>" . $product2->image . "</td>";
+                    echo "<td>" . $product2->planet . "</td>";
+                    echo "<td>" . $product2->radius . "</td>";
+                    echo "<td>" . $product2->inhabitants . "</td>";
+                    echo "<td>$" . $product2->price . "</td>";
+                    echo "</tr>";
+                    $total += $product2->price;
+                 }
+             }
+         }
+         echo "<h3>Your total price will be: $$total</h3>";
+        ?>
+        <form method="post" action="checkout.php">
+            <input type="Submit" value="Continue with Purchase">
+        </form>
 </body>
 
 </html>
