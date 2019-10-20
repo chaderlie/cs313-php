3917e54f10e409a1bc7898069666a670acb3d9a0
Claire Haderlie <clairehaderlie@gmail.com>
2019-10-19 18:00:47 -0600

    Started week05

---
+++ web/week05/family_tree.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Haderlie et al</title>
+    <link rel="stylesheet" type="text/css" href="family_tree.css"/>
+    <style>
+      img.intext {
+      border-radius: 5px;
+      width: 15%;
+      height: auto;
+      margin: 3px 6px 3px 6px;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <a href="laurabelle.html">
+        <img src="finishedLauraLogo.png" alt="Laura Belle's Craftwell Logo" class="logo">
+      </a>
+    </header>
+    <hr />
+    <ul class="menu">
+        <li><a href="laurabelle.html">Home</a></li>
+        <li><a href="productsAssign03.html">Products</a></li>
+        <li><a href="aboutAssign03.html">About</a></li>
+        <li><a href="assign05.html">Purchase</a></li>
+    </ul>
+    <div class="a">
+      <h1 style="font-family:'Yu Gothic'; text-transform:uppercase">Welcome to our little shop</h1>
+      <p>
+        Hello! Welcome to Laura Belle's Craftwell. We are a family-owned company, 
+        here to bring a little light into your life. We specialize in <i>amigurumi</i>, 
+        and we are excited to be announcing that our line of chrocheted accessories 
+        (such as comfy scarfs and cute hats) will be coming soon!
+      </p>
+      <h3 style="font-family:'Yu Gothic'; text-transform: uppercase;">What are Amigurumi?</h3>
+      <img src="frog.jpg" alt="Frog Amigurumi" class="intext" style="float:right;">
+      <p>
+        "Amigurumi" is a Japanese art of knitting or crocheting small, stuffed
+        yarn creatures. The word is a combination of the Japanese words <i>ami</i>,
+        meaning crocheted or knitted, and <i>nuigurumi</i>, meaning stuffed doll.
+        What's not to love?
+          </p>
+      <img src="groupOfAnimals.jpg" alt="Various Amiguri" class="intext" style="float:left;">
+      <p>
+        They're squishy, they're cute, they're safe, they're homemade. We offer
+        a variety of yarn animals that are great for any age. Each yarnimal is
+        carefully crafted by hand to produce the highest amount of cuteness and
+        fun.
+      </p>
+      <p>
+        When you buy our yarnimals, you are helping support the entrepreneurship of a
+        family excited to do more. Laura, the founder of this company, is still in high
+        school, but she is starting her life off right with a new idea that will help kids
+        and parents alike. Thank you for supporting Laura Belle's Craftwell!
+      </p>
+      <div style="text-align: center; font-family: Arial; font-size: 18px;">
+        <a href="productsAssign03.html" class="buttonlike">Products</a>
+        <a href="aboutAssign03.html" class="buttonlike">About</a>
+      </div>
+    </div>
+    <footer>Thank you for visiting!</footer>
+  </body>
+</html>
---
+++ web/week05/homepage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Haderlie et al</title>
+    <link rel="stylesheet" type="text/css" href="family_tree.css"/>
+    <style>
+      img.intext {
+      border-radius: 5px;
+      width: 15%;
+      height: auto;
+      margin: 3px 6px 3px 6px;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <a href="laurabelle.html">
+        <img src="finishedLauraLogo.png" alt="Laura Belle's Craftwell Logo" class="logo">
+      </a>
+    </header>
+    <hr />
+    <ul class="menu">
+        <li><a href="homepage.php">Home</a></li>
+        <li><a href="family_tree.php">Family Tree</a></li>
+        <li><a href="memory_list.php">Memories</a></li>
+        <li><a href="recipes.php">Recipes</a></li>
+    </ul>
+    <div class="a">
+      <h1 style="font-family:'Yu Gothic'; text-transform:uppercase">Welcome to Haderlie et al.</h1>
+      <p>
+          This website was created for Kirk and Katie Haderlie and all their descendents (and kids-in-law, of course).
+          Still working on the content and style of the thing.
+          Should be working good soon!
+    </p>
+      <h3 style="font-family:'Yu Gothic'; text-transform: uppercase;">Who are Kirk and Katie?</h3>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+          </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+      </p>
+      <div style="text-align: center; font-family: Arial; font-size: 18px;">
+        <a href="family_tree.php" class="buttonlike">Family Tree</a>
+        <a href="memory_list.php" class="buttonlike">Memories</a>
+      </div>
+    </div>
+    <footer>Thank you for visiting!</footer>
+  </body>
+</html>
