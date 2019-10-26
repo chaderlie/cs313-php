69084d26ead034ef2de378d8c08fb4000a019769
Claire Haderlie <clairehaderlie@gmail.com>
2019-10-26 17:17:30 -0600

    Working on week 06

--- web/week05/insert_memory.php
+++ web/week05/insert_memory.php
@@ -53,54 +53,7 @@ $memory_date = $_POST['memory_date'];
     <div class="a">
         <h1 style="font-family:'Yu Gothic'; text-transform:uppercase">Thank you!</h1>
 
-        <p>You inserted a new memory with the title: <?php $title ?></p>
-        <form action='insert_memory.php' method='post'>
-            <div class="form-group">
-                <label>Your First Name</label>
-                <input type="text" name="first_name_input" placeholder="First Name">
-            </div>
-            <br />
-            <div class="form-group">
-                <label>Your Last name</label>
-                <input type="text" class="input_text" name="last_name_input" placeholder="Last Name">
-            </div>
-            <br>
-            <br>
-            <label>Story title</label>
-            <input type="text" name="story_title" placeholder="Story title">
-            <div class="form-group">
-                <label><b>Your Story</b></label>
-                <br />
-                <textarea class="form-control" id="story_content" rows="20" cols="100"></textarea>
-            </div>
-            <div>
-                <label>What kind of a story is this?</label> <br />
-                <input type="checkbox" name="attribute_tag[]" value="funny">Funny<br>
-                <input type="checkbox" name="attribute_tag[]" value="spiritual">Spiritual<br>
-                <input type="checkbox" name="attribute_tag[]" value="sneaky">Sneaky<br>
-                <input type="checkbox" name="attribute_tag[]" value="sad">Sad<br>
-                <input type="checkbox" name="attribute_tag[]" value="got_someone_in_trouble">Got-someone-in-trouble<br>
-                <input type="checkbox" name="attribute_tag[]" value="scary">Scary<br>
-            </div>
-            <br />
-            <div>
-                <label for="exampleFormControlSelect2">Please tag family members!</label>
-                <br />
-                <?php
-                foreach ($db->query('SELECT first_name, last_name FROM family_members ORDER BY first_name ASC;') as $row) {
-                    $first = $row['first_name'];
-                    $last = $row['last_name'];
-                    echo "<input type='checkbox' name='tagged_fam_mems[]' value='$first'>$first $last <br />";
-                }
-                ?>
-            </div>
-            <div>
-                <label for="exampleFormControlSelect2">Please give the date (or an estimate)
-                    of when this occured</label>
-                <input type="date" name="memory_date">
-            </div>
-            <br />
-            <input type="submit" value="Add Story">
+        <p>You inserted a new memory with the title: <?php echo ($title); ?></p>
         </form>
         <br />
         <br />
